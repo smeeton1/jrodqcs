94b443956c9d142e9d87de7aed1d4029c7c56e11
smeeton <jeremy.p.rodriguez@mail.mcgill.ca>
2020-01-27 13:33:39 +0000

    added index array creation function

--- jlsrc/parcing.jl
+++ jlsrc/parcing.jl
@@ -1,13 +1,49 @@
 
 module parcing
 
-export Read_InPut
+using ITensors
 
+export Read_InPutFile, Read_InPutLine
 
-function Read_InPut()
+function check_str(a)
+    try
+        parse(Int64,a)
+        true
+    catch
+        false
+    end
+end
+
+function Index_setup(a)
+
+    d=Index[]
+    for i=1:a
+        append!(d,[Index(4),Index(4)])
+    end
+    d=reshape(d,(a,2))
 
 end
 
+function Read_InPutFile(InPutFile)
 
+ a=[]
+ open(InPutFile) do f
+   i=1
+   while !eof(f)
+    x=readline(f)
+    append!(a,[split(x, " ")])
+   end
+ end
+ 
+ 
+ return a
+
+end
+
+function Read_InPutLine()
+
+
+
+end
 
 end
