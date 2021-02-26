608a9437e18321d84d315162006eaff90a2fd601
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2021-02-26 20:33:09 +0000

    added more function for reading openQASM

--- src/openQASM.jl
+++ src/openQASM.jl
@@ -65,6 +65,29 @@ function findChArray(a,s)
     return j
 end
 
+function splitlines(a)
+    i=1
+    m=length(a)
+    while i < m
+        n=findChArray(a[i],';')
+        if  n < length(a[i])
+            b=a[i][1:n]
+            c=a[i][n+1:end]
+            #println(b)
+            #println(c)
+            deleteat!(a[i], n+1:length(a[i]))
+            #println(a[i])
+            insert!(a,i+1,c)
+            #println(a[i+1])
+            m=length(a)
+        end
+        i=i+1
+    end
+    
+    return a
+    
+end
+
 
 function splitequal(a)
     n=length(a)
@@ -159,39 +182,35 @@ end
 function add_gate(a)
     m=findchar(a[1][2],'(')
     A=Name_gate(a[1][2][1:m])
-    k=2
-    if m<length(a[1][2])
-        l=findchar(a[1][2],':')+1
-        j=true
-        if l<length(a[1][2])
-           push!(A.paramaters,a[1][2][l+1:end])
-           k=k+1 
-        else
-           k=k+1 
-        end
-        while j==true
-            if  findchar(a[1][k],')')<length(a[1][k])
-                j=false
-                push!(A.paramaters,a[1][k][1:end-1])
-                k=k+1
-            else
-                push!(A.paramaters,a[1][k])
-                k=k+1
-            end
-        end
-    else
-        k=3
+    k=findChArray(a[1],')')
+    b=compress_string(a[1][2:k])
+    n=findchar(b,'(')
+    l=length(b)
+    go =true
+    while go && n<l
+      s,n1= get_contained_string(b,n,':')
+      if n1<l
+            n=n1
+      end
+      s,n= get_contained_string(b,n,',')
+      push!(A.paramaters,s[1:findchar(s,')')]) 
+      if findchar(s,')')<length(s)
+        go =false
+      end
+      
     end
+
     
-    if a[1][end]=="{"
+    if a[1][end]=="{" || findchar(a[1][end],'{') < length(a[1][end])
         n=length(a[1])-1
     else
         n=length(a[1])
     end
     
-    for i=k:n
+    for i=k+1:n
        push!(A.inputs,a[1][i]) 
     end
+    
     
     i=2
     j=true
@@ -209,5 +228,216 @@ function add_gate(a)
     
     return A
 end
+
+##################################################################
+#                                                                #
+# Functions for working with registar                            #
+#                                                                #
+##################################################################
+
+function set_init(a,r)
+    s=splitequal(a)
+    if s != "0"
+        r.init=s
+    end
+end
+
+function give_qbit(a,r)
+    k=findchar(a,'[')
+    l=findchar(a,']')
+    b = a(1:k)
+    n=0
+    m=0
+    for i=1:length(r)
+        if b == r[i].name
+            m = i
+        end
+    end
+    if m > 1
+        for i=1:m-1
+            n=n+r[i].l
+        end
+    end
+    n=n+parse(Int64,a[k+2:l])
+    return n
+end
+
+
+##################################################################
+#                                                                #
+# Functions to set up gate for network                           #
+#                                                                #
+##################################################################
+
+function get_n(a,r, inv = false )
+    if a[1][1]=='U'
+           i=findChArray(a,')') 
+           b=compress_string(a[1:i])
+            #println(b)
+           m=findchar(b,'(')
+           m2=findchar(b,',')
+           s1=b[m+2:m2]   
+           m3=findchar(b[m2+2:end],',')+m2+1 
+           s2 = b[m2+2:m3]
+           m4 = findchar(b,')')
+           s3 = b[m3+2:m4]
+            #println("s1= ",s1)
+            #println("s2= ",s2)
+            #println("s3= ",s3)
+            #println(a[end][1:findchar(a[end],';')])
+        if !inv
+            n=[give_qbit(a[end][1:findchar(a[end],';')],r),eval(Meta.parse(s1)),eval(Meta.parse(s2)),eval(Meta.parse(s3))]
+        else
+            n=[give_qbit(a[end][1:findchar(a[end],';')],r),-eval(Meta.parse(s1)),-eval(Meta.parse(s2)),eval(Meta.parse(s3))]
+        end
+            
+        else
+          n=[] 
+          for i=2:length(a)
+               push!(n, give_qbit(a[i][1:findchar(a[i],';')],r))#give_qbit(a[i][1:findchar(a[i],';')],r))
+          end 
+        end
+    return n
+end
+
+function apply_g(a,r,g,gate,qbit)
+    if a[1] == "ctrl" || a[1] == "inv" || a[1][1:findchar(a[1],'[')] == "pow"
+        for j=1:length(g)
+            if a[2] == g[j].name
+               if a[1][1:findchar(a[1],'[')] == "pow"
+                   i=findChArray(a,']')
+                   b=compress_string(a[1:i]) 
+                   gate,qbit= apply_ng(a[i:end],r,g[j],gate,qbit,mod= b)
+                    
+                else
+                   gate,qbit= apply_ng(a[2:end],r,g[j],gate,qbit,mod= a[1])
+                end
+            end
+        end
+        if a[1][1:findchar(a[1],'[')] == "pow"
+           i=findChArray(a,']') 
+           b=compress_string(a[1:i])
+           m=eval(Meta.parse(b[findchar(b,'[')+2:findchar(b,']')]))
+            #println(a[i+1:end])
+           for k=1:m
+                apply_g(a[i+1:end],r,g,gate,qbit) 
+           end
+            
+        end
+        
+        if a[1] == "inv"
+           push!(gate,a[2][1:findchar(a[2],'(')]) 
+
+           push!(qbit,get_n(a[2:end],r,true)) 
+        end
+        
+        if a[1] == "ctrl"
+           push!(gate,"c"*a[2][1:findchar(a[2],'(')]) 
+
+           push!(qbit,get_n(a[2:end],r)) 
+        end
+        
+    else
+       push!(gate,a[1][1:findchar(a[1],'(')]) 
+
+       push!(qbit,get_n(a,r))
+    end
+    return gate,qbit
+end
+
+function apply_ng(a,r,g,gate,qbit,mod= "none")
+  #println(a)
+  if mod[1:findchar(mod,'[')] == "pow"
+        m=eval(Meta.parse(mod[findchar(mod,'[')+2:findchar(mod,']')]))
+        for j=1:m-1
+            apply_ng(a,r,g,gate,qbit)
+        end
+        
+  end
+  i=findChArray(a,')') 
+  #println(i)
+
+  if i < length(a)
+    c=compress_string(a[1:i])
+    #println(c)
+    n=findchar(c,'(')
+    #println(n)
+    #println(length(c))
+    #println(length(g.gate))
+    for k=1:length(g.gate)
+      println(g.gate[k])
+      if mod =="inv"
+          b = copy( g.gate[length(g.gate)-k+1])     
+      else
+          b = copy( g.gate[k])
+      end
+      for j=1:length(g.paramaters)
+         s,ch=split_onchar(g.paramaters[j],'=')
+         if n < length(c)
+             s1,n1=get_contained_string(c,n) 
+             s1,h=split_onchar(s1,'=')
+             if h == ""
+                  ch=s1[1:findchar(s1,')')]   
+                  n=n1
+             else
+               if s==s1
+                  ch = h[1:findchar(h,')')]
+                  n = n1        
+               end
+                      
+             end
+         end
+         println(ch)
+         if ch != ""
+             for l =1:length(b)
+                b[l]=replace(b[l],s=>ch)
+             end
+         else
+             println(s," is not defined.")        
+         end
+      end
+      for j=1:length(g.inputs)
+         for l=1:length(b)
+                 b[l]=replace(b[l],g.inputs[j]=>a[j+i][1:findchar(a[j+i],';')])
+         end
+      end
+      gate,qbit = apply_g(b,r,g,gate,qbit)
+      if mod == "inv"
+                gate[end]="i"*gate[end]      
+      end
+    end
+    
+  else
+    for k=1:length(g.gate)
+      if mod =="inv"
+          b = copy( g.gate[length(g.gate)-k+1])     
+      else
+          b = copy( g.gate[k])
+      end
+      if length(g.paramaters)>1
+          for j=1:length(g.paramaters)
+             s,ch=split_onchar(g.paramaters[j],'=')
+             println(s)
+             println(ch)
+             for l=1:length(b)
+                 b[l]=replace(b[l],s=>ch)
+             end
+          end        
+      end
+      for j=1:length(g.inputs)
+         for l=1:length(b)
+             b[l]=replace(b[l],g.inputs[j]=>a[1+j][1:findchar(a[1+j],';')])
+         end
+      end      
+      gate,qbit = apply_g(b,r,g,gate,qbit)
+      if mod == "inv"
+                gate[end]="i"*gate[end]      
+      end
+    end
+  end
+
+  return gate,qbit
+end
+
 
 end
